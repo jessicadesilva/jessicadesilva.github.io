be59734467652251f8aa54806641c24756af36e5
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-11-08 14:28:51 -0800

    Update __init__.py
    - register core_blueprint
    - fix typo in 404 filename

--- website/__init__.py
+++ website/__init__.py
@@ -21,12 +21,14 @@ def create_app():
 
 def register_blueprints(app):
     # Import the blueprints
+    from website.core import core_blueprint
     from website.events import events_blueprint
     from website.groups import groups_blueprint
     from website.research import research_blueprint
 
     # Since the application instance is now created, register each Blueprint
     # with the Flask application instance (app)
+    app.register_blueprint(core_blueprint)
     app.register_blueprint(events_blueprint)
     app.register_blueprint(groups_blueprint)
     app.register_blueprint(research_blueprint)
@@ -35,4 +37,4 @@ def register_blueprints(app):
 def register_error_pages(app):
     @app.errorhandler(404)
     def page_not_found(e):
-        return render_template("404.j2"), 404
+        return render_template("404.html"), 404
