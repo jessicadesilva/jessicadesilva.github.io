3b07732ba8b87937b8e77e31f0dffd1c3fb4e7b9
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-11-13 15:34:55 -0800

    add test_events.py

---
+++ tests/functional/test_events.py
@@ -0,0 +1,16 @@
+def test_get_upcoming_events_page(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/events/upcoming.html' page is requested (GET)
+    THEN check the response is valid
+    """
+    assert test_client.get("/events/upcoming.html").status_code == 200
+
+
+def test_get_past_events_page(test_client):
+    """
+    GIVEN a Flask application configured for testing
+    WHEN the '/events/past.html' page is requested (GET)
+    THEN check the response is valid
+    """
+    assert test_client.get("/events/past.html").status_code == 200
