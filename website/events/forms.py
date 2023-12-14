9c816115f4b8b811feb883ac0955838befa9603f
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-12-14 11:20:42 -0800

    Update `events` module
    CHANGED
    * replaced JSON datafile with database support.
    * replaced markdown support with WYSIWYG editor.

---
+++ website/events/forms.py
@@ -0,0 +1,66 @@
+# -*- coding: utf-8 -*-
+"""Event forms."""
+from flask_wtf import FlaskForm
+from flask_ckeditor import CKEditorField
+from wtforms import DateField
+from wtforms.validators import DataRequired
+
+from .models import Event, EventStatus
+from .validator import EventValidator
+from .utils import determine_event_status
+from website.utils import strip_p_tags
+
+
+class EventForm(FlaskForm):
+    """Add an event form."""
+
+    start_date = DateField("Start Date", validators=[DataRequired()])
+    end_date = DateField("End Date", validators=[DataRequired()])
+    name = CKEditorField("Event Name", validators=[DataRequired()])
+    description = CKEditorField("Event Description", validators=[DataRequired()])
+
+    def __init__(self, *args, **kwargs):
+        """Create instance."""
+        super(EventForm, self).__init__(*args, **kwargs)
+
+    def validate(self, **kwargs):
+        """Validate the form and ensure the event does not already exist."""
+
+        initial_validation = super(EventForm, self).validate()
+
+        if not initial_validation:
+            return False
+
+        if self.start_date.data > self.end_date.data:
+            self.start_date.errors.append(
+                "Start date must be before or equal to end date."
+            )
+            return False
+
+        event = EventValidator(
+            status=determine_event_status(self.end_date.data),
+            start_date=self.start_date.data,
+            end_date=self.end_date.data,
+            name=strip_p_tags(self.name.data),
+            description=strip_p_tags(self.description.data),
+        )
+
+        try:
+            EventValidator.model_validate(event)
+        except ValueError as error:
+            self.name.errors.append(f"{error}")
+            return False
+
+        new_event = Event(
+            status_id=EventStatus.return_status_id(event.status),
+            start_date=event.start_date,
+            end_date=event.end_date,
+            name=event.name,
+            description=event.description,
+        )
+
+        if Event.search_for_event(new_event) is not None:
+            self.name.errors.append("Event already exists.")
+            return False
+
+        return True
---
+++ website/events/routes.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+"""Events module, including scheduled and past events."""
+from datetime import datetime
+
+from flask import Blueprint, flash, render_template, request, redirect, url_for
+
+from website.extensions import database
+from website.utils import strip_p_tags, formatted_date
+from website.events.forms import EventForm
+from website.events.models import Event, EventStatus
+from website.events.utils import determine_event_status
+
+
+blueprint = Blueprint(
+    "events", __name__, url_prefix="/events", static_folder="../static"
+)
+
+
+@blueprint.route("/future.html")
+def future_events():
+    """Display future events."""
+
+    data = []
+
+    for event in database.session.execute(database.select(Event)).scalars().all():
+        # Update the status of the event if it has changed
+        # This is necessary because the status of an event is determined by its end date
+        if determine_event_status(event.end_date) == "completed":
+            event.update(
+                commit=True, status_id=EventStatus.return_status_id("completed")
+            )
+
+        if event.event_status_rel.status == "scheduled":
+            output_data = {
+                "ID": event.id,
+                "Date": formatted_date(event.end_date)
+                if event.start_date == event.end_date
+                else formatted_date(event.start_date)
+                + " - "
+                + formatted_date(event.end_date),
+                "Name": event.name,
+                "Description": event.description,
+            }
+            data.append(output_data)
+
+    return render_template("events/future_events.j2", data=data)
+
+
+@blueprint.route("/past.html")
+def past_events():
+    """Display past events."""
+
+    data = []
+
+    for event in database.session.execute(database.select(Event)).scalars().all():
+        # Update the status of the event if it has changed
+        # This is necessary because the status of an event is determined by its end date
+        if determine_event_status(event.end_date) == "completed":
+            event.update(
+                commit=True, status_id=EventStatus.return_status_id("completed")
+            )
+
+        if event.event_status_rel.status == "completed":
+            output_data = {
+                "ID": event.id,
+                "Date": formatted_date(event.end_date)
+                if event.start_date == event.end_date
+                else formatted_date(event.start_date)
+                + " - "
+                + formatted_date(event.end_date),
+                "Name": event.name,
+                "Description": event.description,
+            }
+            data.append(output_data)
+
+    return render_template("events/past_events.j2", data=data)
+
+
+@blueprint.route("/add_event.html", methods=["GET", "POST"])
+def add_event():
+    """Add an event."""
+
+    form = EventForm()
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            Event.create(
+                status_id=EventStatus.return_status_id(
+                    determine_event_status(form.end_date.data)
+                ),
+                start_date=form.start_date.data,
+                end_date=form.end_date.data,
+                name=strip_p_tags(form.name.data),
+                description=strip_p_tags(form.description.data),
+            )
+            flash(f"{strip_p_tags(form.name.data)} successfully added.", "success")
+            return redirect(request.url)
+
+    return render_template("events/add_event.j2", form=form)
+
+
+@blueprint.route("/edit_event/<int:event_id>.html", methods=["GET", "POST"])
+def edit_event(event_id: int):
+    """Edit an event."""
+
+    form = EventForm()
+    event = Event.get_by_id(event_id)
+
+    if request.method == "POST":
+        if form.validate_on_submit():
+            event.update(
+                commit=True,
+                status_id=EventStatus.return_status_id(
+                    determine_event_status(form.end_date.data)
+                ),
+                start_date=form.start_date.data,
+                end_date=form.end_date.data,
+                name=strip_p_tags(form.name.data),
+                description=strip_p_tags(form.description.data),
+            )
+            flash(f"{strip_p_tags(form.name.data)} successfully updated.", "success")
+            return redirect(request.url)
+
+    form.start_date.data = datetime.strptime(event.start_date, "%Y-%m-%d").date()
+    form.end_date.data = datetime.strptime(event.end_date, "%Y-%m-%d").date()
+    form.name.data = event.name
+    form.description.data = event.description
+
+    return render_template("events/edit_event.j2", form=form)
+
+
+@blueprint.route("/delete_event/<int:event_id>.html", methods=["GET", "POST"])
+def delete_event(event_id: int):
+    """Delete an event."""
+
+    event = Event.get_by_id(event_id)
+
+    if request.method == "POST":
+        event.delete()
+        flash(f"{event.name} successfully deleted.", "success")
+        return redirect(url_for("events.future_events"))
+
+    form = EventForm()
+    flash(f"Are you sure you want to delete {event.name}?", "warning")
+
+    return render_template("events/delete_event.j2", event=event, form=form)
