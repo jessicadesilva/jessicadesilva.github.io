fb8d7eab3e9eebbf4a2076700ec5ba046480e6f1
August Frisk <114228717+4N0NYM0U5MY7H@users.noreply.github.com>
2023-12-17 16:47:38 -0800

    create `dev_utils` decorator
    This decorator creates additional functionality for specific routes throughout the application. Namely, we don't want to generate the CRUD interface for the static generated webpages on the live site. These should only be accessible during development.
    
    The decorator is designed in a way to easily add features as needed.

--- website/utils.py
+++ website/utils.py
@@ -1,6 +1,21 @@
 # -*- coding: utf-8 -*-
 """Helper utilities and decorators."""
+from functools import wraps
+
 import bleach
+from flask import current_app
+
+
+def dev_utils(func):
+    """Decorator that adds addtional functionality when in debug mode."""
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if current_app.debug:
+            kwargs["debug"] = True
+        return func(*args, **kwargs)
+
+    return wrapper
 
 
 def formatted_date(date: str) -> str:
