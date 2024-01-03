543d103956e997d74f768fb3b56167808952704f
August Frisk <friska@oregonstate.edu>
2024-01-03 00:48:06 -0800

    update research projects validator

--- tests/unit/test_project_validators.py
+++ tests/unit/test_project_validators.py
@@ -7,70 +7,6 @@ from website.research.validator import ProjectValidator
 
 
 class TestProjectValidator:
-    def test_project_validator_accepts_valid_data_with_all_fields(self):
-        project = ProjectValidator(
-            type_id=1,
-            status_id=1,
-            image="default.jpg",
-            advisors="Test Advisor",
-            students="Test Student",
-            majors="Test Major",
-            title="Test Project",
-            description="Test Description",
-            link="https://www.csustan.edu/",
-        )
-        assert isinstance(project, ProjectValidator)
-
-    def test_project_validator_accepts_valid_data_with_required_fields(self):
-        project = ProjectValidator(
-            type_id=1,
-            status_id=1,
-            students="Test Student",
-            title="Test Project",
-            description="Test Description",
-        )
-        assert isinstance(project, ProjectValidator)
-
-    def test_type_id_must_be_greater_than_0_returns_type_id(self):
-        assert ProjectValidator.type_id_must_be_greater_than_0(1) == 1
-
-    def test_status_id_must_be_greater_than_0_returns_status_id(self):
-        assert ProjectValidator.status_id_must_be_greater_than_0(1) == 1
-
-    def setup_validator_with_all_fields(self, **kwargs):
-        type_id = 1
-        status_id = 1
-        image = "default.jpg"
-        advisors = "Test Advisor"
-        students = "Test Student"
-        majors = "Test Major"
-        title = "Test Project"
-        description = "Test Description"
-        link = "https://www.csustan.edu/"
-
-        if kwargs:
-            type_id = kwargs.get("type_id")
-            status_id = kwargs.get("status_id")
-            image = kwargs.get("image")
-            advisors = kwargs.get("advisors")
-            students = kwargs.get("students")
-            majors = kwargs.get("majors")
-            title = kwargs.get("title")
-            description = kwargs.get("description")
-            link = kwargs.get("link")
-
-        return ProjectValidator(
-            type_id=type_id,
-            status_id=status_id,
-            image=image,
-            advisors=advisors,
-            students=students,
-            majors=majors,
-            title=title,
-            description=description,
-            link=link,
-        )
-
     def setup_validator_with_required_fields(self, **kwargs):
         type_id = 1
         status_id = 1
@@ -93,16 +29,56 @@ class TestProjectValidator:
             description=description,
         )
 
-    def test_class_project_validator_raises_validation_error_with_invalid_type_id(
-        self,
-    ):
-        with pytest.raises(ValidationError) as error:
-            self.setup_validator_with_required_fields(type_id=0)
-        assert "type_id must be greater than 0" in str(error.value)
+    def setup_validator_with_all_fields(self, **kwargs):
+        _validator = self.setup_validator_with_required_fields(**kwargs)
+        type_id = _validator.type_id
+        status_id = _validator.status_id
+        students = _validator.students
+        title = _validator.title
+        description = _validator.description
+        image = "default.jpg"
+        advisors = "Test Advisor"
+        majors = "Test Major"
+        link = "https://www.csustan.edu/"
 
-    def test_class_project_validator_raises_validation_error_with_invalid_status_id(
+        if kwargs:
+            image = kwargs.get("image")
+            advisors = kwargs.get("advisors")
+            majors = kwargs.get("majors")
+            link = kwargs.get("link")
+
+        return ProjectValidator(
+            type_id=type_id,
+            status_id=status_id,
+            image=image,
+            advisors=advisors,
+            students=students,
+            majors=majors,
+            title=title,
+            description=description,
+            link=link,
+        )
+
+    def test_project_validator_accepts_valid_data_with_required_fields(self):
+        project = self.setup_validator_with_required_fields()
+        assert isinstance(project, ProjectValidator)
+
+    def test_project_validator_accepts_valid_data_with_all_fields(self):
+        project = self.setup_validator_with_all_fields()
+        assert isinstance(project, ProjectValidator)
+
+    def test_id_must_be_greater_than_0_returns_id(self):
+        assert ProjectValidator.id_must_be_greater_than_0(1) == 1
+
+    def test_project_validator_raises_validation_error_with_invalid_id(
         self,
     ):
-        with pytest.raises(ValidationError) as error:
-            self.setup_validator_with_required_fields(status_id=0)
-        assert "status_id must be greater than 0" in str(error.value)
+        test_fields = [
+            {"type_id": 0},
+            {"status_id": 0},
+        ]
+
+        for field in test_fields:
+            with pytest.raises(ValidationError) as error:
+                self.setup_validator_with_all_fields(**field)
+            assert "ID must be greater than 0" in str(error.value)
